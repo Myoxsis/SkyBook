f3b35ac232ed18ebbcc6e0999d04e6bd0c3cd6b8
maxime_alain <69240270+Myoxsis@users.noreply.github.com>
2025-05-24 16:27:19 +0200

    Infer airline from flight number

--- lib/data/airline_data.dart
+++ lib/data/airline_data.dart
@@ -1,16 +1,20 @@
 import '../models/airline.dart';
 
 const List<Airline> airlines = [
-  Airline(name: 'Air France', callsign: 'AFR'),
-  Airline(name: 'British Airways', callsign: 'BAW'),
-  Airline(name: 'Emirates', callsign: 'UAE'),
-  Airline(name: 'Lufthansa', callsign: 'DLH'),
-  Airline(name: 'Delta Air Lines', callsign: 'DAL'),
-  Airline(name: 'American Airlines', callsign: 'AAL'),
-  Airline(name: 'United Airlines', callsign: 'UAL'),
-  Airline(name: 'Qantas', callsign: 'QFA'),
+  Airline(name: 'Air France', callsign: 'AFR', code: 'AF'),
+  Airline(name: 'British Airways', callsign: 'BAW', code: 'BA'),
+  Airline(name: 'Emirates', callsign: 'UAE', code: 'EK'),
+  Airline(name: 'Lufthansa', callsign: 'DLH', code: 'LH'),
+  Airline(name: 'Delta Air Lines', callsign: 'DAL', code: 'DL'),
+  Airline(name: 'American Airlines', callsign: 'AAL', code: 'AA'),
+  Airline(name: 'United Airlines', callsign: 'UAL', code: 'UA'),
+  Airline(name: 'Qantas', callsign: 'QFA', code: 'QF'),
 ];
 
 final Map<String, Airline> airlineByName = {
   for (final a in airlines) a.name: a,
+};
+
+final Map<String, Airline> airlineByCode = {
+  for (final a in airlines) a.code: a,
 };
--- lib/models/airline.dart
+++ lib/models/airline.dart
@@ -1,8 +1,9 @@
 class Airline {
   final String name;
   final String callsign;
+  final String code;
 
-  const Airline({required this.name, required this.callsign});
+  const Airline({required this.name, required this.callsign, required this.code});
 
   String get display => name;
 }
