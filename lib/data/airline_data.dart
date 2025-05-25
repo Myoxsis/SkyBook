0b0651171f9f717bcf23813e892b4612ca9e5422
maxime_alain <69240270+Myoxsis@users.noreply.github.com>
2025-05-25 17:21:32 +0200

    Add additional airlines

--- lib/data/airline_data.dart
+++ lib/data/airline_data.dart
@@ -14,15 +14,26 @@ const List<Airline> airlines = [
   Airline(name: 'Alaska Airlines', callsign: 'ASA', code: 'AS'),
   Airline(name: 'JetBlue', callsign: 'JBU', code: 'B6'),
   Airline(name: 'Southwest Airlines', callsign: 'SWA', code: 'WN'),
+  Airline(name: 'Spirit Airlines', callsign: 'NKS', code: 'NK'),
+  Airline(name: 'Frontier Airlines', callsign: 'FFT', code: 'F9'),
+  Airline(name: 'WestJet', callsign: 'WJA', code: 'WS'),
   // Europe
   Airline(name: 'KLM', callsign: 'KLM', code: 'KL'),
   Airline(name: 'Swiss', callsign: 'SWR', code: 'LX'),
   Airline(name: 'Turkish Airlines', callsign: 'THY', code: 'TK'),
+  Airline(name: 'Ryanair', callsign: 'RYR', code: 'FR'),
+  Airline(name: 'easyJet', callsign: 'EZY', code: 'U2'),
+  Airline(name: 'Aer Lingus', callsign: 'EIN', code: 'EI'),
   // Asia & Oceania
   Airline(name: 'Singapore Airlines', callsign: 'SIA', code: 'SQ'),
   Airline(name: 'All Nippon Airways', callsign: 'ANA', code: 'NH'),
   Airline(name: 'Air New Zealand', callsign: 'ANZ', code: 'NZ'),
   Airline(name: 'Qatar Airways', callsign: 'QTR', code: 'QR'),
+  Airline(name: 'Cathay Pacific', callsign: 'CPA', code: 'CX'),
+  Airline(name: 'Air India', callsign: 'AIC', code: 'AI'),
+  Airline(name: 'EVA Air', callsign: 'EVA', code: 'BR'),
+  Airline(name: 'Etihad Airways', callsign: 'ETD', code: 'EY'),
+  Airline(name: 'Ethiopian Airlines', callsign: 'ETH', code: 'ET'),
 ];
 
 final Map<String, Airline> airlineByName = {
